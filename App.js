757e8c3dab307352db3293c64a7aa0c4646c3550
ilkerkaran <ilkerkaran@gmail.com>
2020-04-29 02:12:24 +0100

    main scren component implemented

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Main from './screens/Main';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
+      <Main />
     </View>
   );
 }
