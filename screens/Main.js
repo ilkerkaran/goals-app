1dd24a5f97fa1f08225777c4003d49b1929a2385
ilkerkaran <ilkerkaran@gmail.com>
2020-04-29 02:11:48 +0100

    main screen component added

---
+++ screens/Main.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Modal, Button, View, StyleSheet } from 'react-native';
+import GoalList from '../components/GoalList';
+import GoalInput from '../components/GoalInput'
+
+const INITIAL_GOALS = [{
+  id: 1,
+  title: "Learn Docker"
+},
+{
+  id: 2,
+  title: "Learn React Native"
+}]
+const main = () => {
+  const [goals, setGoals] = useState(INITIAL_GOALS);
+  const [isAddingActive, setIsAddingActive] = useState(false);
+
+  const onModalOKHandler = (goal) => {
+    setIsAddingActive(false);
+    setGoals([...goals, goal])
+  }
+
+  const onModalCancelHandler = () => {
+    setIsAddingActive(false);
+  }
+
+  const onAddGoalHandler = () => {
+    setIsAddingActive(true);
+  }
+
+  const onGoalDeleteHandler = (id) => {
+    setGoals(goals.filter(g => g.id != id))
+
+  }
+  return (
+    <View style={styles.main}>
+      <Modal visible={isAddingActive} animationType={"slide"}>
+        <View style={styles.main}>
+          <GoalInput onPressOK={onModalOKHandler} onPressCancel={onModalCancelHandler} />
+        </View>
+      </Modal>
+      <View style={styles.main}>
+        <Button onPress={onAddGoalHandler} title="ADD GOAL" />
+        <GoalList goals={goals} onGoalDelete={onGoalDeleteHandler} />
+      </View>
+    </View>
+
+  );
+}
+
+
+export default main;
+
+
+
+const styles = StyleSheet.create({
+  main: {
+    justifyContent: "center",
+    flex: 1
+  }
+
+
+})
