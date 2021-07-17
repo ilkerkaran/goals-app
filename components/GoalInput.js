c707bce34eaca42b9b56a355c8b0011f84f8956b
ilker <ilkerkaran@gmail.com>
2021-07-17 15:39:42 +0100

    disabled add button when no text entered

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,16 +1,17 @@
-import React, { useState } from 'react';
-import { Button, TextInput, View, StyleSheet } from 'react-native';
+import React, { useState } from 'react'
+import {
+  Button, TextInput, View, StyleSheet
+} from 'react-native'
 
 const goalInput = ({ onPressOK, onPressCancel }) => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState('')
 
-
-  const onTextChangeHandler = (text) => {
-    setText(text);
+  const onTextChangeHandler = (textInput) => {
+    setText(textInput)
   }
   const onOKHandler = () => {
     onPressOK({ id: Math.random(), title: text })
-    setText('');
+    setText('')
   }
 
   return (
@@ -19,28 +20,41 @@ const goalInput = ({ onPressOK, onPressCancel }) => {
         style={styles.text}
         value={text}
         placeholder="Enter your goal here!"
-        onChangeText={onTextChangeHandler} />
+        onChangeText={onTextChangeHandler}
+      />
       <View style={styles.buttons}>
         <View style={styles.button}>
-          <Button title="CANCEL" color="red" onPress={onPressCancel} /></View>
-        <View style={styles.button}><Button title="ADD" color="green" onPress={onOKHandler} /></View>
+          <Button
+            title="CANCEL"
+            color="red"
+            onPress={onPressCancel}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            disabled={!text}
+            title="ADD"
+            color="green"
+            onPress={onOKHandler}
+          />
+
+        </View>
       </View>
     </View>
   )
 }
 
-export default goalInput;
-
+export default goalInput
 
 const styles = StyleSheet.create({
   goalInput: {
-    width: "80%"
+    width: '80%'
   },
   buttons: {
-    width: "100%",
+    width: '100%',
     marginTop: 20,
-    flexDirection: "row",
-    justifyContent: "space-evenly"
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
   },
   button: {
     flex: 1,
@@ -49,4 +63,4 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 48
   }
-});
+})
